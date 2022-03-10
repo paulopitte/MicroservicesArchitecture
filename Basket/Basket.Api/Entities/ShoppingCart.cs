e1c7f5c735481512f8f513b99c51e4cdaa9410f1
Paulo Pitte <paulopitte@gmail.com>
2022-03-10 13:00:52 -0300

    add ShoppingCart

---
+++ Basket/Basket.Api/Entities/ShoppingCart.cs
@@ -0,0 +1,36 @@
+﻿namespace Basket.Api.Entities
+{
+    public struct ShoppingCart
+    {
+
+        public ShoppingCart()
+        {
+
+        }
+
+
+        public ShoppingCart(string name) =>
+            UserName = name;
+
+        public string UserName { get; set; }
+
+
+        public List<ShoppingCartItem> Items { get; set; } = new List<ShoppingCartItem>();
+
+
+        public decimal TotalPrice
+        {
+            get
+            {
+                decimal totalprice = 0;
+                foreach (var item in Items)
+                    totalprice += item.Price * item.Quantity;
+
+                return TotalPrice;
+            }
+        }
+
+
+
+    }
+}
---
+++ Basket/Basket.Api/Entities/ShoppingCartItem.cs
@@ -0,0 +1,8 @@
+﻿namespace Basket.Api.Entities
+{
+    public class ShoppingCartItem
+    {
+        public int Quantity { get; internal set; }
+        public decimal Price { get; internal set; }
+    }
+}
