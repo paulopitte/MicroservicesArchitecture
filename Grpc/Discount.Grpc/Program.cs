2eec03d5a8d5c42af63fe20c3b2949a6f572bfd3
Paulo Pitte <paulopitte@gmail.com>
2022-03-15 15:59:33 -0300

    implementando grpc

--- Grpc/Discount.Grpc/Program.cs
+++ Grpc/Discount.Grpc/Program.cs
@@ -1,4 +1,4 @@
-using Discount.Grpc.Services;
+using Discount.Grpc.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,10 +8,11 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddGrpc();
 
+builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
-app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+ app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
